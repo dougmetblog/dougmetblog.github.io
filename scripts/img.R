9ee366db169218b6959dcd53bc176817247f9e5e
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-03-19 10:31:43 +0000

    img script

--- scripts/img.R
+++ scripts/img.R
@@ -20,36 +20,53 @@ download_image <- function(img, root, assets) {
   local
 }
 
+# Detect right type of image
 is_local_img <- function(img, pattern = "blogspot\\.com") {
   src <- rvest::html_attr(img, "src")
   grepl(pattern, src)
 }
 
+# Gets all the standalone images
+local_img <- function(html, pattern = "blogspot\\.com") {
+  
+  imgs <- html_nodes(html, "img")
+  imgs[vapply(imgs, is_local_img, logical(1), pattern = pattern)]
+}
+
+
+# Detect a tag with right type of image inside
 is_local_img_a <- function(a, pattern = "blogspot\\.com") {
   
   imgs <- html_nodes(a, "img")
+  imgs <- imgs[vapply(imgs, is_local_img, logical(1), pattern = pattern)]
   if(length(imgs)>1) {
     stop("An a tag had more than one image!")
   }
   isTRUE(length(imgs)==1)
 }
 
+# Gets all the a tags with images inside
+local_img_a <- function(html, pattern = "blogspot\\.com") {
+  
+  a <- html_nodes(html, "a")
+  a[vapply(a, is_local_img_a, logical(1), pattern = pattern)]
+}
+
+
+
+# Start of real script ----------------------------------------------------
+
+# Read in html
 bpost <- read_html(html_file)
 
-a <- bpost %>% html_nodes("a")
+# First process a tags with images inside
+a <- bpost %>% local_img_a()
 
-imgs <- bpost %>%
-  html_nodes("img")
-
-img_src <- imgs %>%
-  html_attr("src")
+# Then process images that are on their own (left overs)
+imgs <- bpost %>% local_img()
 
 
+# Something like this
 img <- imgs[[1]]
-if(is_local_img(img, "blogspot.com")) {
-  # Download the image and return local path
-  local <- download_image(img, root, assets)
-  # Update the source
-  xml2::xml_attr(img, "src") <- local
-}
-  
+local <- download_image(img, root, assets)
+xml2::xml_attr(img, "src") <- local
