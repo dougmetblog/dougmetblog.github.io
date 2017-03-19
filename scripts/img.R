186e2dce236856b8571685310007778cbaa56432
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-03-19 21:22:51 +0000

    Uses entirely xml2 and xpath

--- scripts/img.R
+++ scripts/img.R
@@ -1,72 +1,73 @@
-library(rvest)
-library(stringr)
 library(xml2)
-library(purrr)
+
+# Parameters ---------------------------------------------------------------
 
 root = "~/Web/jeknew2"
 assets = "/assets/img"
 
+# Functions ---------------------------------------------------------------
+
+download_image <- function(nd, root, assets, attrib = "src") {
+  # Grab the URL
+  url <- xml2::xml_attr(nd, attrib)
+  # The last two paths are what we want
+  url_split <- unlist(strsplit(url, split = "/"))
+  url2 <- tail(url_split,2)
+  # Relative to assets location
+  localurl <- file.path(assets, paste(url2, collapse = "/"))
+  # Create the directory to put it in
+  pth <- file.path(root, assets, url2[1])
+  dir.create(pth, recursive = TRUE, showWarnings = FALSE)
+  # Download and link the new url
+  download.file(url, file.path(root, localurl))
+  xml2::xml_attr(nd, attrib) <- localurl
+  # Return the URL incase they want it
+  localurl
+}
+
+# Strip yaml header
+split_yaml_html <- function(html_file) {
+  
+  hlines <- readLines(html_file, warn = FALSE)
+  dashes <- cumsum(grepl("^---", hlines))
+  yheader <- 1:which(dashes == 2)[1]
+  tmpfile <- tempfile(fileext = ".html")
+  on.exit(unlink(tmpfile))
+  
+  writeLines(hlines[-yheader], tmpfile)
+  
+  list(yaml = hlines[yheader],
+       html = xml2::read_html(tmpfile))
+}
+
+# Get html as lines
+get_html_lines <- function(hnode) {
+  body <- xml2::xml_find_first(hnode, "//body")
+  tmpfile <- tempfile(fileext = ".html")
+  on.exit(unlink(tmpfile))
+  write_xml(body, tmpfile)
+  html_lines <- readLines(tmpfile, warn = FALSE)
+  # Strip off the body tags
+  html_lines[!grepl("body>$", html_lines)]
+}
+
+
+# The main script ---------------------------------------------------------
+
+# Read in
 html_file <- "~/Web/jeknew2/_posts/2012-04-25-renormalisation-group.html"
 
-download_image <- function(img, root, assets) {
-  
-  path <- rvest::html_attr(img, "src")
-  path_split <- unlist(strsplit(path, split = "/"))
-  path2 <- tail(path_split,2)
-  local <- file.path(assets, paste(path2, collapse = "/"))
-  pth <- file.path(root, assets, path2[1])
-  dir.create(pth, recursive = TRUE, showWarnings = FALSE)
-  download.file(path, file.path(root, local))
-  local
-}
+bpost <- split_yaml_html(html_file)
 
-# Detect right type of image
-is_local_img <- function(img, pattern = "blogspot\\.com") {
-  src <- rvest::html_attr(img, "src")
-  grepl(pattern, src)
-}
-
-# Gets all the standalone images
-local_img <- function(html, pattern = "blogspot\\.com") {
-  
-  imgs <- html_nodes(html, "img")
-  imgs[vapply(imgs, is_local_img, logical(1), pattern = pattern)]
-}
-
-
-# Detect a tag with right type of image inside
-is_local_img_a <- function(a, pattern = "blogspot\\.com") {
-  
-  imgs <- html_nodes(a, "img")
-  imgs <- imgs[vapply(imgs, is_local_img, logical(1), pattern = pattern)]
-  if(length(imgs)>1) {
-    stop("An a tag had more than one image!")
-  }
-  isTRUE(length(imgs)==1)
-}
-
-# Gets all the a tags with images inside
-local_img_a <- function(html, pattern = "blogspot\\.com") {
-  
-  a <- html_nodes(html, "a")
-  a[vapply(a, is_local_img_a, logical(1), pattern = pattern)]
-}
-
-
-
-# Start of real script ----------------------------------------------------
-
-# Read in html
-bpost <- read_html(html_file)
-
-# First process a tags with images inside
-a <- bpost %>% local_img_a()
-
-# Then process images that are on their own (left overs)
-imgs <- bpost %>% local_img()
-
+# Parse
+imgs <- xml_find_all(bpost$html, "//a/img[contains(@src, 'blogspot')]")
+aimgs <- xml_find_all(bpost$html, "//a/img[contains(@src, 'blogspot')]/ancestor::a")
 
 # Something like this
-img <- imgs[[1]]
-local <- download_image(img, root, assets)
-xml2::xml_attr(img, "src") <- local
+locals <- sapply(imgs, download_image, root = root, assets = assets, attrib = "src")
+localsa <- sapply(aimgs, download_image, root = root, assets = assets, attrib = "href")
+
+bpost$html_parsed <- get_html_lines(bpost$html)
+
+# Write out
+writeLines(c(bpost$yaml, "", bpost$html_parsed), "test.html")
